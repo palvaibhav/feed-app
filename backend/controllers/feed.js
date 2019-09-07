a59d349c6a0e4a1e2cbc67b378369b9fa8bbabed
palvaibhav <vaibhav171997@gmail.com>
2019-09-07 20:50:09 +0530

    adding auth checks

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -123,6 +123,11 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized.');
+        error.statusCode = 403;
+        throw error;
+      }
       if (imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl);
       }
@@ -147,6 +152,11 @@ exports.deletePost = (req, res, next) => {
       if (!post) {
         const error = new Error('Could not find the post.');
         error.statusCode = 404;
+        throw error;
+      }
+      if (post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized.');
+        error.statusCode = 403;
         throw error;
       }
       // check logged in user
