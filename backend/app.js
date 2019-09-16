01118d280e87039a7e41766357fd44ff22409e96
palvaibhav <vaibhav171997@gmail.com>
2019-09-17 00:22:30 +0530

    extracting user data from auth token

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const graphqlHttp = require('express-graphql');
 
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolvers = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
 
 const env = require('./env');
 
@@ -53,6 +54,8 @@ app.use((req, res, next) => {
   }
   next();
 });
+
+app.use(auth);
 
 app.use(
   '/graphql',
